3ffb855b586909006be12b69bdda2455616c0979
Cong Cuong PHAM <cuong.pham@gct-solution.net>
2024-12-19 03:12:59 +0100

    add d3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { D3VizComponent } from './d3-visualization/d3-visualization.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'd3-viz', pathMatch: 'full' },
+  { path: 'd3-viz', component: D3VizComponent },
+];
